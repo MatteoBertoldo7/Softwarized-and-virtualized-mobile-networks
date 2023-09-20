954cd2fc99294efa5755103488fbd1468e4cc9db
MatteoBertoldo <89697695+MatteoBertoldo7@users.noreply.github.com>
2023-09-20 18:14:33 +0200

    Update publicsafety.py

--- publicsafety.py
+++ publicsafety.py
@@ -1,13 +1,20 @@
-# Codice per la slice "Smart Traffic"
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
 
-class SmartTrafficSlice(app_manager.RyuApp):
+class PublicSafetySlice(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SmartTrafficSlice, self).__init__(*args, **kwargs)
+        super(PublicSafetySlice, self).__init__(*args, **kwargs)
 
-        # Imposta le politiche di QoS per la slice Smart Traffic
-        self.qos_params = {"bw": 100, "delay": "3ms"}
+        # Imposta le politiche di QoS per la slice Public Safety
+        self.qos_params = {"bw": 50, "delay": "20ms"}
 
         # Inizializza altre variabili e configurazioni specifiche della slice
 
@@ -16,10 +23,9 @@ class SmartTrafficSlice(app_manager.RyuApp):
         # Gestisci i pacchetti in base alle politiche di QoS
         # Implementa le regole di flusso necessarie per garantire la QoS specificata
 
-    # Altre funzioni specifiche per la slice Smart Traffic
+    # Altre funzioni specifiche per la slice Public Safety
 
 if __name__ == '__main__':
-    # Configura e avvia l'applicazione Ryu per la slice Smart Traffic
+    # Configura e avvia l'applicazione Ryu per la slice Public Safety
     from ryu.cmd import manager
     manager.main(['ryu.app.ofctl_rest', '--observe-links'])
-
