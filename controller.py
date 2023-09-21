7887bb0bb5de51dfa55391c0776ea7ae7387afb2
MatteoBertoldo <89697695+MatteoBertoldo7@users.noreply.github.com>
2023-09-21 11:57:25 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -5,6 +5,7 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet
 import logging
+import time
 
 class SDNController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
